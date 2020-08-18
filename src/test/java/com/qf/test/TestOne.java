44fc2122416fb5f1d52b81e0d5bac92f29838ad6
冯薪朵 <296981829@qq.com>
2020-08-18 16:18:18 +0800

    李轲 ：包+pow+properties

--- src/test/java/com/qf/test/TestOne.java
+++ src/test/java/com/qf/test/TestOne.java
@@ -1,6 +1,6 @@
 package com.qf.test;
 
-import com.qf.pojo.User;
+import com.qf.exam.pojo.admin;
 import org.junit.Test;
 
 public class TestOne {
@@ -9,7 +9,7 @@ public class TestOne {
     public void test1(){
 
         System.out.println("hello world");
-        System.out.println(new User());
+        System.out.println(new admin());
 
     }
 
