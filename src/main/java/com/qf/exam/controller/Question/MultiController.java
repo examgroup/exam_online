3babbf094dff9db205543b87523e1951d5590028
冯薪朵 <296981829@qq.com>
2020-08-19 03:08:32 +0800

    李轲：完成 15个接口 试题库的功能

---
+++ src/main/java/com/qf/exam/controller/Question/MultiController.java
@@ -0,0 +1,53 @@
+package com.qf.exam.controller.Question;
+
+import com.qf.exam.pojo.Multi;
+import com.qf.exam.service.MultiService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class MultiController {
+    @Resource
+    private MultiService multiService;
+
+    @GetMapping("Multi")
+    List<Multi> getAll(){
+      return multiService.getAll();
+    }
+
+    @GetMapping("/MultiByID/{questionId}")
+    Multi getMultiByID(@PathVariable("questionId") int questionId){
+      return   multiService.getMultiByID(questionId);
+    }
+
+    @GetMapping("saveMulti")
+    String save(@RequestBody Multi multi){
+        boolean save = multiService.save(multi);
+        if(save){
+            return "1";
+        }else {
+            return "0";
+        }
+    }
+    @GetMapping("/deleteMulti/{questionId}")
+    String delete(@PathVariable("questionId") int questionId){
+        boolean delete = multiService.delete(questionId);
+        if(delete){
+            return "1";
+        }else {
+            return "0";
+        }
+    }
+
+    @PostMapping("updateMulti")
+    String update(@RequestBody  Multi multi){
+        boolean update = multiService.update(multi);
+        if(update){
+            return "1";
+        }else {
+            return "0";
+        }
+    }
+}
