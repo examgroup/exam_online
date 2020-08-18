816fda5abef35208c8f85f7dbc806b131267125f
冯薪朵 <296981829@qq.com>
2020-08-18 16:16:51 +0800

    李轲 ：包+pow+properties

---
+++ src/main/java/com/qf/exam/pojo/admin.java
@@ -0,0 +1,16 @@
+package com.qf.exam.pojo;
+
+
+import lombok.Data;
+
+@Data
+public class admin {
+    private int adminId;//ID
+    private String adminName;//姓名
+    private String sex;//年龄
+    private String tel;//电话
+    private String email;//邮箱
+    private String pwd;//密码
+    private String cardID;//身份证
+    private String role;//角色
+}
